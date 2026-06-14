6389711995cff23820d98b299312f67344e27560
Priyadarshini <cpwebbuilder@gmail.com>
2026-06-14 20:43:56 +0530

    Updated footer with premium icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-import Loader from "@/components/Loader";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
@@ -19,69 +16,41 @@ import Stats from "@/components/Stats";
 import BookingForm from "@/components/BookingForm";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1800);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
   return (
     <main className="relative overflow-hidden">
-
-      {/* Background Image */}
-    
-
       <Navbar />
 
       <Hero />
 
-      <FadeUp>
+      
         <About />
-      </FadeUp>
+     
 
       <Marquee />
 
-      <FadeUp>
+      
         <Services />
-      </FadeUp>
+     
 
-      <FadeUp>
+      
         <Transformations />
-      </FadeUp>
+     
 
-      <FadeUp>
-        <Testimonials />
-      </FadeUp>
+      <Testimonials />
 
-      <FadeUp>
-        <Stats />
-      </FadeUp>
+      <Stats />
 
-      <FadeUp>
-        <BookingForm />
-      </FadeUp>
+      <BookingForm />
 
-      <FadeUp>
+      
         <ReelsWall />
-      </FadeUp>
+     
 
-
-      <FadeUp>
-        <Contact />
-      </FadeUp>
+      <Contact />
 
       <Footer />
 
       <WhatsAppButton />
-
     </main>
   );
 }
